86073a62eb89ab59f4ad929b6b6f063a5507baae
ombeniweb <ombenimangai83@gmail.com>
2026-04-02 12:38:07 +0300

    project3

--- methodist-app/src/App.jsx
+++ methodist-app/src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const addTask = () => {
+    setList([...list, task]);
+    setTask("");
+  };
+
   return (
     <div>
-      <h1>Login</h1>
-      <input type="email" placeholder="Email" />
-      <br />
-      <input type="password" placeholder="Password" />
-      <br />
-      <button>Login</button>
+      <h1>Todo App</h1>
+      <input
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
